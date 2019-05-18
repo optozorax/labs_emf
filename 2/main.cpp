86f972843351791cb2b9b7f494ee03c1de844c27
Sheprut Ilya <optitel223@gmail.com>
2019-05-19 02:56:32 +0700

    Сделана 1 таблица

--- 2/main.cpp
+++ 2/main.cpp
@@ -3,6 +3,8 @@
 #include <functional>
 #include <iterator>
 #include <iomanip>
+#include <fstream>
+#include <sstream>
 #include <Eigen/Dense>
 
 using namespace std;
@@ -10,12 +12,12 @@ using namespace std;
 typedef Eigen::MatrixXd Matrix;
 typedef Eigen::VectorXd Vector;
 
-typedef std::function<double(double)> Function1D;
-typedef std::function<double(double, double)> Function2D;
-typedef std::function<double(double, double, double)> Function3D;
+typedef function<double(double)> Function1D;
+typedef function<double(double, double)> Function2D;
+typedef function<double(double, double, double)> Function3D;
 
 //-----------------------------------------------------------------------------
-double integral_gauss3(const std::vector<double>& X, Function1D f) {
+double integral_gauss3(const vector<double>& X, Function1D f) {
 	const double x1 = -sqrt(3.0/5.0);
 	const double x2 = 0;
 	const double x3 = -x1;
@@ -40,7 +42,7 @@ double integral_gauss3(const std::vector<double>& X, Function1D f) {
 }
 
 //-----------------------------------------------------------------------------
-void make_grid(std::vector<double>& X, double a, double b, long n) {
+void make_grid(vector<double>& X, double a, double b, long n) {
 	X.clear();
 	double h = (b-a)/n;
 	for (double i = 0; i <= n; ++i)
@@ -79,6 +81,77 @@ double basicFunctionGrad(double left, double middle, double right, double x) {
 	return 0;
 }
 
+//-----------------------------------------------------------------------------
+Function1D calcFirstDerivative(const Function1D& f) {
+	return [f](double x) -> double {
+		const double h = 0.001;
+		return (-f(x + 2 * h) + 8 * f(x + h) - 8 * f(x - h) + f(x - 2 * h)) / (12 * h);
+	};
+}
+
+//-----------------------------------------------------------------------------
+Function2D calcRightPart(
+	const Function1D& lambda, 
+	const Function2D& u, 
+	const Function1D& sigma
+) {
+	// f = -div(lambda(u) * grad u) + sigma * du/dt
+	return [=](double x, double t) -> double {
+		using namespace placeholders;
+		auto ut = calcFirstDerivative(bind(u, x, _1));
+		auto ux = calcFirstDerivative(bind(u, _1, t));
+		auto lambda_grad = [lambda, ux, u](double x, double t) -> double {
+			return lambda(u(x, t)) * ux(x);
+		};
+		auto div = calcFirstDerivative(bind(lambda_grad, _1, t));
+		return -div(x) + sigma(x) * ut(x);
+	};
+}
+
+//-----------------------------------------------------------------------------
+double norm(const vector<double>& a, const vector<double>& b) {
+	assert(a.size() == b.size());
+	double sum = 0;
+	for (int i = 0; i < a.size(); i++)
+		sum += (a[i] - b[i])*(a[i] - b[i]);
+	return sqrt(sum);
+}
+
+//-----------------------------------------------------------------------------
+ostream& operator<<(ostream& out, const vector<double>& mas) {
+	for (auto& i : mas)
+		out << i << " ";
+	return out;
+}
+
+//-----------------------------------------------------------------------------
+string write_for_latex_double(double v, int precision) {
+	int power = log(std::fabs(v)) / log(10.0);
+	double value = v / pow(10.0, power);
+
+	if (v != v) return "nan";
+
+	if (v == 0) {
+		power = 0;
+		value = 0;
+	}
+
+	stringstream sout;
+	sout.precision(precision);
+	if (power == -1 || power == 0 || power == 1) {
+		sout << v;
+	} else {
+		sout << value << "\\cdot 10^{" << power << "}";
+	}
+
+	return sout.str();
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
 class lin_approx_t
 {
 public:
@@ -108,21 +181,21 @@ public:
 	}
 
 	double value(double pos) const {
-		int start = std::distance(x.begin(), std::lower_bound(x.begin(), x.end(), pos))-1;
+		int start = distance(x.begin(), lower_bound(x.begin(), x.end(), pos))-1;
 		if (start == -1) {
-			if (std::fabs(pos-x[0]) < 0.00001)
+			if (fabs(pos-x[0]) < 0.00001)
 				return q[0];
 			else
 				return 0;
 		}
 		if (start == x.size()) {
-			if (std::fabs(pos-x.back()) < 0.00001)
+			if (fabs(pos-x.back()) < 0.00001)
 				return q.back();
 			else
 				return 0;
 		}
 		double sum = 0;
-		for (int i = start; i < std::min<int>(start+2, x.size()); ++i)
+		for (int i = start; i < min<int>(start+2, x.size()); ++i)
 		//for (int i = 0; i < x.size(); ++i)
 			sum += q[i] * basic(pos, i);
 		return sum;
@@ -137,52 +210,61 @@ public:
 	}
 };
 
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
 double calc_a1_integral(const Function1D& lambda, const lin_approx_t& u, int i, int j) {
-	if (std::abs(i - j) > 1)
+	if (abs(i - j) > 1)
 		return 0;
 	auto f = [&] (double x) -> double {
 		return lambda(u.value(x)) * u.basic_grad(x, i) * u.basic_grad(x, j);
 	};
-	std::vector<double> X;
+	vector<double> X;
 	if (i != j)
-		make_grid(X, std::min(u.middle(i), u.middle(j)), std::min(u.right(i), u.right(j)), 10);
+		make_grid(X, min(u.middle(i), u.middle(j)), min(u.right(i), u.right(j)), 10);
 	else
 		make_grid(X, u.left(i), u.right(i), 10);
 	return integral_gauss3(X, f);
 }
 
+//-----------------------------------------------------------------------------
 double calc_a2_integral(const Function1D& sigma, const lin_approx_t& u, int i, int j) {
-	if (std::abs(i - j) > 1)
+	if (abs(i - j) > 1)
 		return 0;
 	auto f = [&] (double x) -> double {
 		return sigma(x) * u.basic(x, i) * u.basic(x, j);
 	};
-	std::vector<double> X;
+	vector<double> X;
 	if (i != j)
-		make_grid(X, std::min(u.middle(i), u.middle(j)), std::min(u.right(i), u.right(j)), 10);
+		make_grid(X, min(u.middle(i), u.middle(j)), min(u.right(i), u.right(j)), 10);
 	else
 		make_grid(X, u.left(i), u.right(i), 10);
 	return integral_gauss3(X, f);	
 }
 
+//-----------------------------------------------------------------------------
 double calc_b1_integral(const Function1D& f, const lin_approx_t& u, int i) {
 	auto fun = [&] (double x) -> double {
 		return f(x) * u.basic(x, i);
 	};
-	std::vector<double> X;
+	vector<double> X;
 	make_grid(X, u.left(i), u.right(i), 10);
 	return integral_gauss3(X, fun);	
 }
 
+//-----------------------------------------------------------------------------
 double calc_b2_integral(const Function1D& sigma, const lin_approx_t& u, const lin_approx_t& u_last_time, int i) {
 	auto fun = [&] (double x) -> double {
 		return sigma(x) * u_last_time.value(x) * u.basic(x, i);
 	};
-	std::vector<double> X;
+	vector<double> X;
 	make_grid(X, u.left(i), u.right(i), 10);
 	return integral_gauss3(X, fun);	
 }
 
+//-----------------------------------------------------------------------------
 lin_approx_t approximate_function(Function1D lambda, const lin_approx_t& u) {
 	lin_approx_t result = u;
 	for (int i = 0; i < result.q.size(); ++i)
@@ -190,6 +272,7 @@ lin_approx_t approximate_function(Function1D lambda, const lin_approx_t& u) {
 	return result;
 }
 
+//-----------------------------------------------------------------------------
 Matrix calcA(const lin_approx_t& u_last, double dt, Function1D lambda, Function1D sigma) {
 	Matrix result(u_last.size(), u_last.size());
 	for (int i = 0; i < u_last.size()-1; ++i) {
@@ -200,6 +283,8 @@ Matrix calcA(const lin_approx_t& u_last, double dt, Function1D lambda, Function1
 
 	return result;
 } /// Рассчитать матрицу A(q)
+
+//-----------------------------------------------------------------------------
 Vector calcB(const lin_approx_t& u_last, double dt, Function1D f, Function1D sigma, const lin_approx_t& u_last_time) {
 	Vector result(u_last.size());
 	for (int i = 0; i < u_last.size(); ++i) {
@@ -209,6 +294,7 @@ Vector calcB(const lin_approx_t& u_last, double dt, Function1D f, Function1D sig
 	return result;
 } /// Рассчитать вектор правой части b(q)
 
+//-----------------------------------------------------------------------------
 void write_first_boundary_conditions(Matrix& A, Vector& b, const lin_approx_t& u, Function2D u_true, double time) {
 	A(0, 0) = 1;
 	for (int i = 1; i < A.cols(); ++i) A(0, i) = 0;
@@ -219,6 +305,11 @@ void write_first_boundary_conditions(Matrix& A, Vector& b, const lin_approx_t& u
 	b(b.rows()-1) = u_true(u.x.back(), time);
 }
 
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
 struct Result
 {
 	enum ExitType {
@@ -233,6 +324,7 @@ struct Result
 	double residual;
 };
 
+//-----------------------------------------------------------------------------
 ostream& operator<<(ostream& out, const Result::ExitType& exit) {
 	switch (exit) {
 		case Result::EXIT_RESIDUAL: out << "residual"; break;
@@ -243,6 +335,7 @@ ostream& operator<<(ostream& out, const Result::ExitType& exit) {
 	return out;
 }
 
+//-----------------------------------------------------------------------------
 Result solveFixedPointIteration(
 	Function2D f,
 	Function2D u_true,
@@ -264,12 +357,8 @@ Result solveFixedPointIteration(
 	result.iterations = 0;
 	while (true) {
 		auto A = calcA(u, dt, lambda, sigma);
-		auto b = calcB(u, dt, std::bind(f, std::placeholders::_1, time), sigma, u_last_time);
+		auto b = calcB(u, dt, bind(f, placeholders::_1, time), sigma, u_last_time);
 		write_first_boundary_conditions(A, b, u, u_true, time);
-
-		//cout << "x:" << endl << last_x << endl;
-		//cout << "A: " << endl << A << endl;
-		//cout << "b: " << endl << b << endl;
 
 		Eigen::JacobiSVD<Matrix> svd(A, Eigen::ComputeThinU | Eigen::ComputeThinV);
 		Vector x = svd.solve(b);
@@ -285,7 +374,7 @@ Result solveFixedPointIteration(
 		
 		{
 			auto A1 = calcA(u, dt, lambda, sigma);
-			auto b1 = calcB(u, dt, std::bind(f, std::placeholders::_1, time), sigma, u_last_time);
+			auto b1 = calcB(u, dt, bind(f, placeholders::_1, time), sigma, u_last_time);
 			write_first_boundary_conditions(A1, b1, u, u_true, time);
 
 			double au = (A1 * x - b1).norm();
@@ -310,54 +399,23 @@ Result solveFixedPointIteration(
 	return result;
 } /// Решить уравнение методом простой итерации
 
-Function1D calcFirstDerivative(const Function1D& f) {
-	return [f](double x) -> double {
-		const double h = 0.001;
-		return (-f(x + 2 * h) + 8 * f(x + h) - 8 * f(x - h) + f(x - 2 * h)) / (12 * h);
-	};
+lin_approx_t calcTrulyApprox(const vector<double>& grid, const Function2D& u_true, double time) {
+	lin_approx_t u_truly_approx;
+	u_truly_approx.x = grid;
+	u_truly_approx.q = grid;
+	for (int i = 0; i < u_truly_approx.x.size(); i++)
+		u_truly_approx.q[i] = u_true(u_truly_approx.x[i], time);
+	return u_truly_approx;
 }
 
-Function2D calcRightPart(
-	const Function1D& lambda, 
-	const Function2D& u, 
-	const Function1D& sigma
-) {
-	// f = -div(lambda(u) * grad u) + sigma * du/dt
-	return [=](double x, double t) -> double {
-		using namespace std::placeholders;
-		auto ut = calcFirstDerivative(std::bind(u, x, _1));
-		auto ux = calcFirstDerivative(std::bind(u, _1, t));
-		auto lambda_grad = [lambda, ux, u](double x, double t) -> double {
-			return lambda(u(x, t)) * ux(x);
-		};
-		auto div = calcFirstDerivative(std::bind(lambda_grad, _1, t));
-		return -div(x) + sigma(x) * ut(x);
-	};
-}
-
-double norm(const std::vector<double>& a, const std::vector<double>& b) {
-	assert(a.size() == b.size());
-	double sum = 0;
-	for (int i = 0; i < a.size(); i++)
-		sum += (a[i] - b[i])*(a[i] - b[i]);
-	return sqrt(sum);
-}
-
-ostream& operator<<(ostream& out, const std::vector<double>& mas) {
-	for (auto& i : mas)
-		out << i << " ";
-	return out;
-}
-
+//-----------------------------------------------------------------------------
 void write_iterations_information(double t0, const Result& result, const Function2D& u_true) {
 	cout << "time: " << t0 << endl;
 	cout << "residual: " << result.residual << endl;
 	cout << "iterations: " << result.iterations << endl;
 	cout << "answer:   " << result.answer.q << endl;
 
-	lin_approx_t u_truly_approx = result.answer;
-	for (int j = 0; j < u_truly_approx.x.size(); j++)
-		u_truly_approx.q[j] = u_true(u_truly_approx.x[j], t0);
+	lin_approx_t u_truly_approx = calcTrulyApprox(result.answer.x, u_true, t0);
 	cout << "shold be: " << u_truly_approx.q << endl;
 
 	cout << "norm: " << norm(u_truly_approx.q, result.answer.q) << endl;
@@ -365,7 +423,8 @@ void write_iterations_information(double t0, const Result& result, const Functio
 	cout << endl << "---------------------------" << endl;
 }
 
-void solveByTime(
+//-----------------------------------------------------------------------------
+vector<Result> solveByTime(
 	Function2D f,
 	Function2D u_true,
 	Function1D lambda,
@@ -375,40 +434,177 @@ void solveByTime(
 	double eps,
 	double maxiter
 ) {
+	vector<Result> res;
 	lin_approx_t u = u_start;
 
 	for (int i = 0; i < time_grid.size()-1; ++i) {
 		double t0 = time_grid[i];
 		double t1 = time_grid[i+1];
 		auto result = solveFixedPointIteration(f, u_true, lambda, sigma, u, t1-t0, t0, eps, maxiter);
+		res.push_back(result);
 
 		// Выводим мета-информацию
-		write_iterations_information(t0, result, u_true);
+		// write_iterations_information(t0, result, u_true);
 
 		u = result.answer;
 	}
+
+	return res;
 }
 
-int main() {
-	auto u_true = [] (double x, double t) -> double { return 3*x*x + exp(t); };
-	auto lambda = [] (double u) -> double { return u; };
-	auto sigma = [] (double x) -> double { return 1; };
-	auto f = calcRightPart(lambda, u_true, sigma);
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 
-	lin_approx_t u;
-	make_grid(u.x, 0, 1, 10);
-	for (int i = 0; i < u.x.size(); i++)
-		u.q.push_back(u_true(u.x[i], 0));
-		//u.q.push_back(u_true(u.x[i], 0)-0.01);
-		//u.q.push_back(1);
+template<class A, class B, class C>
+struct triple {
+	A first;
+	B second;
+	C third;
+};
 
-	/*auto result = solveFixedPointIteration(f, u_true, lambda, sigma, u, 0.001, 0, 0.001, 1000);
-	write_iterations_information(0, result, u_true); */
+//-----------------------------------------------------------------------------
+void writeFirstInvestigation() {
+	vector<triple<Function2D, string, bool>> us;
+	us.push_back({[] (double x, double t) -> double { return 3*x + t; }, "$3x+t$", false});
+	us.push_back({[] (double x, double t) -> double { return 2*x*x + t; }, "$2x^2+t$", false});
+	us.push_back({[] (double x, double t) -> double { return x*x*x + t; }, "$x^3+t$", false});
+	us.push_back({[] (double x, double t) -> double { return x*x*x*x + t; }, "$x^4+t$", false});
+	us.push_back({[] (double x, double t) -> double { return exp(x) + t; }, "$e^x+t$", false});
+	us.push_back({[] (double x, double t) -> double { return 3*x + t*t; }, "$3x+t^2$", true});
+	us.push_back({[] (double x, double t) -> double { return 3*x + t*t*t; }, "$3x+t^3$", true});
+	us.push_back({[] (double x, double t) -> double { return 3*x + exp(t); }, "$3x+e^t$", true});
+	us.push_back({[] (double x, double t) -> double { return 3*x + sin(t); }, "$3x+sin(t)$", true});
+	us.push_back({[] (double x, double t) -> double { return exp(x) + t*t; }, "$e^x+t^2$", true});
+	us.push_back({[] (double x, double t) -> double { return exp(x) + t*t*t; }, "$e^x+t^3$", true});
+	us.push_back({[] (double x, double t) -> double { return exp(x) + exp(t); }, "$e^x+e^t$", true});
+	us.push_back({[] (double x, double t) -> double { return exp(x) + sin(t); }, "$e^x+sin(t)$", true});
+
+	vector<pair<Function1D, string>> lambdas;
+	lambdas.push_back({[] (double u) -> double { return 1; }, "$1$"});
+	lambdas.push_back({[] (double u) -> double { return u; }, "$u$"});
+	lambdas.push_back({[] (double u) -> double { return u*u; }, "$u^2$"});
+	lambdas.push_back({[] (double u) -> double { return u*u + 1; }, "$u^2+1$"});
+	lambdas.push_back({[] (double u) -> double { return u*u*u; }, "$u^3$"});
+	lambdas.push_back({[] (double u) -> double { return u*u*u*u; }, "$u^4$"});
+	lambdas.push_back({[] (double u) -> double { return exp(u); }, "$e^u$"});
+	lambdas.push_back({[] (double u) -> double { return sin(u); }, "sinu"});
+
+	double a = 1, b = 2, n = 10; // Характеристики сетки по пространству
+	double at = 0, bt = 1, nt = 10; // Характеристики сетки по времени
+
+	ofstream fout("first.txt");
+	fout << "a\t";
+	for (auto& i : lambdas) fout << i.second << ((i.second != lambdas.back().second) ? "\t" : "");
+	fout << endl;
 
 	vector<double> time;
-	make_grid(time, 0, 1, 100);
+	make_grid(time, at, bt, nt);
 	time.erase(time.begin());
-	solveByTime(f, u_true, lambda, sigma, u, time, 1e-7, 100);
+	time.push_back(time[1]-time[0] + bt);
+	for (auto& i : us) {
+		fout << i.second << "\t";
+		auto u_true = i.first;
+		auto sigma = [] (double x) -> double { return 1; };
 
-	system("pause");
+		lin_approx_t u;
+		make_grid(u.x, a, b, n);
+		for (int j = 0; j < u.x.size(); j++)
+			if (i.third)
+				u.q.push_back(u_true(u.x[j], at));
+			else
+				u.q.push_back(1);
+
+		lin_approx_t u_truly_approx = calcTrulyApprox(u.x, u_true, bt);
+
+		for (auto& j : lambdas) {
+			auto lambda = j.first;
+
+			auto result = solveByTime(calcRightPart(lambda, u_true, sigma), u_true, lambda, sigma, u, time, 0.001, 500);
+
+			int itersum = 0;
+			for (auto& k : result) itersum += k.iterations;
+
+			double residual = norm(u_truly_approx.q, result.back().answer.q);
+
+			fout << "\\scalebox{.55}{\\tcell{$" << itersum << "$\\\\$" << write_for_latex_double(residual, 2) << "$}}" << ((j.second != lambdas.back().second) ? "\t" : "");
+		}
+		fout << endl;
+	}
+
+	fout.close();
 }
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+int main() {
+	// auto u_true = [] (double x, double t) -> double { return 3*x*x + exp(t); };
+	// auto lambda = [] (double u) -> double { return u; };
+	// auto sigma = [] (double x) -> double { return 1; };
+	// auto f = calcRightPart(lambda, u_true, sigma);
+
+	// lin_approx_t u;
+	// make_grid(u.x, 0, 1, 10);
+	// for (int i = 0; i < u.x.size(); i++)
+	// 	u.q.push_back(u_true(u.x[i], 0));
+	// 	//u.q.push_back(u_true(u.x[i], 0)-0.01);
+	// 	//u.q.push_back(1);
+
+	// /*auto result = solveFixedPointIteration(f, u_true, lambda, sigma, u, 0.001, 0, 0.001, 1000);
+	// write_iterations_information(0, result, u_true); */
+
+	// vector<double> time;
+	// make_grid(time, 0, 1, 100);
+	// time.erase(time.begin());
+	// solveByTime(f, u_true, lambda, sigma, u, time, 1e-7, 100);
+
+	// system("pause");
+	
+
+	writeFirstInvestigation();
+
+	//writeGridInvestigation();
+
+	//system("pause");
+}
+
+/* Исследования:
+	По вертикали - функция u, по горизонтали - функция лямбда, в клетках - число итераций и итоговая норма
+		Функции: 
+			3*x + t
+			2*x*x + t
+			x*x*x + t
+			x*x*x*x + t
+			exp(x) + t
+			3*x + t*t
+			3*x + t*t*t
+			3*x + exp(t)
+			3*x + sin(t)
+
+		Лямбды:
+			1
+			u
+			u*u
+			u*u + 1
+			u*u*u
+			exp(u)
+
+	Далее для какой-нибудь функции тестируется уменьшение сетки в два, четыре и т. д. раз
+
+	Тестируется неравномерное разбиение по пространству для функций:
+		x*x
+
+		сетка:
+		увеличивающаяся по коэффициенту, график для этого построить
+		f^(-1)
+
+		exp(x)
+		сетка:
+		коэфф
+		1/exp(x)
+
+		x*x*x
+
+ */ 
