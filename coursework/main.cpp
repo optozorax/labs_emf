880d5324033ae3535efd9207c304db68c26c5565
Sheprut Ilya <optitel223@gmail.com>
2019-06-02 02:02:15 +0700

    Оно работает

--- coursework/main.cpp
+++ coursework/main.cpp
@@ -13,6 +13,89 @@ typedef function<double(double)> function_1d_t;
 typedef function<double(double, double)> function_2d_t;
 typedef function<double(double, double, double)> function_3d_t;
 
+//-----------------------------------------------------------------------------
+double calc_integral_gauss3(
+	double a, double b, int n, // n - количество внутренных узлов
+	const function_1d_t& f
+) {
+	const double x1 = -sqrt(3.0/5.0);
+	const double x2 = 0;
+	const double x3 = -x1;
+	const double q1 = 5.0/9.0;
+	const double q2 = 8.0/9.0;
+	const double q3 = q1;
+	double sum = 0;
+	double xk = 0;
+	double h = (b-a)/double(n+1);
+	double h2 = h/2.0;
+	
+	for (int i = 0; i < n+1; ++i) {
+		xk = a + h*i + h2;
+		sum += q1 * f(xk + x1 * h2);
+		sum += q2 * f(xk + x2 * h2);
+		sum += q3 * f(xk + x3 * h2);
+	}
+
+	sum *= h;
+	sum /= 2.0;
+	return sum;
+}
+
+//-----------------------------------------------------------------------------
+double calc_integral_gauss3(
+	double ax, double bx, int nx,
+	double ay, double by, int ny,
+	const function_2d_t& f
+) {
+	return calc_integral_gauss3(ax, bx, nx, [ay, by, ny, f](double x)->double {
+		return calc_integral_gauss3(ay, by, ny, bind(f, x, _1));
+	});
+}
+
+//-----------------------------------------------------------------------------
+double basic_function(double left, double middle, double right, double x) {
+	// Базовая линейная функция
+	if (x > left && x < right) {
+		if (x < middle) {
+			if (middle != left)
+				return (x-left)/(middle-left);
+			else
+				return 0;
+		} else {
+			if (right != middle)
+				return 1-(x-middle)/(right-middle);
+			else
+				return 0;
+		}
+	}
+
+	return 0;
+}
+
+//-----------------------------------------------------------------------------
+double basic_function_grad(double left, double middle, double right, double x) {
+	// Базовая линейная функция
+	if (x > left && x < right) {
+		if (x < middle) {
+			if (middle != left)
+				return 1.0/(middle-left);
+			else
+				return 0;
+		} else {
+			if (right != middle)
+				return -1.0/(right-middle);
+			else
+				return 0;
+		}
+	}
+
+	return 0;
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
 /** Вес, домноженный на базовую функцию. Из сумм этих элементов образуется конечный элемент. */
 struct basic_elem_t
 {
@@ -57,7 +140,7 @@ struct constants_t
 //-----------------------------------------------------------------------------
 matrix_t calc_local_matrix_g(
 	const elem_t& e, 
-	const constants_t& c
+	const constants_t& cs
 ) {
 	double hx = e.get_hx();
 	double hy = e.get_hy();
@@ -73,12 +156,12 @@ matrix_t calc_local_matrix_g(
 		1, 2, -1, -2,
 		-2, -1, 2, 1,
 		-1, -2, 1, 2;
-	result = c.lambda/6.0*(hy/hx*a + hx/hy*b);
+	result = cs.lambda/6.0*(hy/hx*a + hx/hy*b);
 	return result;
 }
 
 //-----------------------------------------------------------------------------
-matrix_t calc_local_matrix_m(
+matrix_t calc_local_matrix_c(
 	const elem_t& e
 ) {
 	double hx = e.get_hx();
@@ -95,74 +178,58 @@ matrix_t calc_local_matrix_m(
 }
 
 //-----------------------------------------------------------------------------
+matrix_t calc_local_matrix_m(
+	const elem_t& e,
+	const constants_t& cs
+) {
+	return cs.gamma * calc_local_matrix_c(e);
+}
+
+//-----------------------------------------------------------------------------
 vector_t calc_local_vector_b(
 	const elem_t& e,
 	const function_2d_t& f
 ) {
-	double hx = e.get_hx();
-	double hy = e.get_hy();
-	matrix_t c(4, 4);
-	c <<
-		4, 2, 2, 1,
-		2, 4, 1, 2,
-		2, 1, 4, 2,
-		1, 2, 2, 4;
 	vector_t fv(4);
 	fv << 
 		f(e.e[0]->x, e.e[0]->y),
 		f(e.e[1]->x, e.e[1]->y),
 		f(e.e[2]->x, e.e[2]->y),
 		f(e.e[3]->x, e.e[3]->y);
-	vector_t result = hx*hy/36.0*c*fv;
-	return result;	
+	return calc_local_matrix_c(e) * fv;	
 }
 
 //-----------------------------------------------------------------------------
-matrix_t calc_global_matrix_g(
-	const vector<elem_t>& es, 
-	const constants_t& c,
-	int n
-) {
-	matrix_t result(n, n);
-	for (auto& e : es) {
-		auto g = calc_local_matrix_g(e, c);
-		for (int i = 0; i < 4; ++i) {
-			for (int j = 0; j < 4; ++j) {
-				result(e.e[i]->i, e.e[j]->i) = g(i, j);
-			}
-		}
-	}
-	return result;
-}
-
-//-----------------------------------------------------------------------------
-matrix_t calc_global_matrix_m(
+matrix_t calc_global_vector(
 	const vector<elem_t>& es,
-	int n
-) {
-	matrix_t result(n, n);
-	for (auto& e : es) {
-		auto m = calc_local_matrix_m(e);
-		for (int i = 0; i < 4; ++i) {
-			for (int j = 0; j < 4; ++j) {
-				result(e.e[i]->i, e.e[j]->i) = m(i, j);
-			}
-		}
-	}
-	return result;
-}
-
-//-----------------------------------------------------------------------------
-vector_t calc_global_vector_b(
-	const vector<elem_t>& es,
-	const function_2d_t& f,
+	const function<vector_t(const elem_t&)> calc_local_vector,
 	int n
 ) {
 	vector_t result(n);
+	result.fill(0);
 	for (auto& e : es) {
-		auto b = calc_local_vector_b(e, f);
+		auto b = calc_local_vector(e);
 		for (int i = 0; i < 4; ++i) {
-			result(e.e[i]->i) = b(i);
+			result(e.e[i]->i) += b(i);
+		}
+	}
+	return result;
+}
+
+//-----------------------------------------------------------------------------
+matrix_t calc_global_matrix(
+	const vector<elem_t>& es,
+	const function<matrix_t(const elem_t&)> calc_local_matrix,
+	int n
+) {
+	matrix_t result(n, n);
+	result.fill(0);
+	for (auto& e : es) {
+		auto m = calc_local_matrix(e);
+		for (int i = 0; i < 4; ++i) {
+			for (int j = 0; j < 4; ++j) {
+				result(e.e[i]->i, e.e[j]->i) += m(i, j);
+			}
 		}
 	}
 	return result;
@@ -170,6 +237,7 @@ vector_t calc_global_vector_b(
 
 //-----------------------------------------------------------------------------
 void calc_crank_nicolson_method(
+	const matrix_t& c,
 	const matrix_t& m,
 	const matrix_t& g,
 	const vector_t& b0, // b current (b_j)
@@ -177,16 +245,18 @@ void calc_crank_nicolson_method(
 	const vector_t& bll, // b last last (b_{j-2})
 	const vector_t& ql, // q last (q_{j-1})
 	const vector_t& qll, // q last last (q_{j-2})
-	const constants_t& c,
+	const constants_t& cs,
 	double dt,
 	matrix_t& a,
 	vector_t& b
 ) {
-	a = (c.chi/dt/dt + c.sigma/2.0/dt) * m + 0.5 * g;
-	b = 0.5 * (b0 + bll) - m * (c.chi/dt/dt * (-2.0*ql + qll) - c.sigma/2.0/dt * qll) - 0.5*g * qll;
+	// Схема Кранка-Николсона
+	a = 0.5 * m + (cs.chi/dt/dt + cs.sigma/2.0/dt) * c + 0.5 * g;
+	b = 0.5 * (b0 + bll) - m * (cs.chi/dt/dt * (-2.0*ql + qll) - cs.sigma/2.0/dt * qll) - 0.5*g * qll - 0.5*m * qll;
 
-	//a = m * ((c.chi / dt / dt) + c.sigma / 2 / dt);
-	//b = bl - m * (-2.0*c.chi/dt/dt * ql + c.chi/dt/dt * qll - c.sigma/2.0/dt * qll) - g*ql;
+	// Моя простая схема
+	//a = g  + m + (cs.chi/dt/dt + cs.sigma/2/dt) * c;
+	//b = b0 + c * (2*cs.chi/dt/dt * ql + (cs.sigma/2/dt - cs.chi/dt/dt) * qll);
 }
 
 //-----------------------------------------------------------------------------
@@ -279,7 +349,7 @@ function_1d_t calc_first_derivative(const function_1d_t& f) {
 //-----------------------------------------------------------------------------
 function_1d_t calc_second_derivative(const function_1d_t& f) {
 	return [f](double x) -> double {
-		const double h = 0.0001;
+		const double h = 0.001;
 		return (-f(x+2*h) + 16*f(x+h) - 30*f(x) + 16*f(x-h) - f(x-2*h))/(12*h*h);
 	};
 }
@@ -287,7 +357,7 @@ function_1d_t calc_second_derivative(const function_1d_t& f) {
 //-----------------------------------------------------------------------------
 function_3d_t calc_right_part(
 	const function_3d_t& u,
-	const constants_t& c
+	const constants_t& cs
 ) {
 	// f = -div(lambda * grad u) + gamma * u + sigma * du/dt + chi * d^2 u/dt^2
 	return [=](double x, double y, double t) -> double {
@@ -298,7 +368,7 @@ function_3d_t calc_right_part(
 		auto uyy = calc_second_derivative(bind(u, x, _1, t));
 		auto utt = calc_second_derivative(bind(u, x, y, _1));
 
-		return -c.lambda * (uxx(x) + uyy(y)) + c.gamma * u(x, y, t) + c.sigma * ut(t) + c.chi * utt(t);
+		return -cs.lambda * (uxx(x) + uyy(y)) + cs.gamma * u(x, y, t) + cs.sigma * ut(t) + cs.chi * utt(t);
 	};
 }
 
@@ -323,15 +393,16 @@ vector<vector_t> solve_differential_equation(
 	const boundary_setter_t& set_boundary_conditions,
 	const vector_t& q0,
 	const vector_t& q1,
-	const constants_t& c,
+	const constants_t& cs,
 	const grid_t& grid,
 	int nt, double dt, double at
 ) {
-	auto m = calc_global_matrix_m(grid.es, grid.bn);
-	auto g = calc_global_matrix_g(grid.es, c, grid.bn);
+	auto c = calc_global_matrix(grid.es, calc_local_matrix_c, grid.bn);
+	auto m = calc_global_matrix(grid.es, bind(calc_local_matrix_m, _1, cs), grid.bn);
+	auto g = calc_global_matrix(grid.es, bind(calc_local_matrix_g, _1, cs), grid.bn);
 
-	vector_t bll = calc_global_vector_b(grid.es, bind(f, _1, _2, at), grid.bn);
-	vector_t bl = calc_global_vector_b(grid.es, bind(f, _1, _2, at+dt), grid.bn);
+	vector_t bll = calc_global_vector(grid.es, bind(calc_local_vector_b, _1, function_2d_t(bind(f, _1, _2, at))), grid.bn);
+	vector_t bl = calc_global_vector(grid.es, bind(calc_local_vector_b, _1, function_2d_t(bind(f, _1, _2, at+dt))), grid.bn);
 	vector_t b0;
 
 	vector_t qll = q0;
@@ -342,14 +413,17 @@ vector<vector_t> solve_differential_equation(
 	result.push_back(q0);
 	result.push_back(q1);
 
-	matrix_t a;
-	vector_t b;
+	matrix_t a(grid.bn, grid.bn);
+	vector_t b(grid.bn);
 	for (int i = 2; i < nt; ++i) {
 		double t = at + i*dt;
-		b0 = calc_global_vector_b(grid.es, bind(f, _1, _2, t), grid.bn);
-		calc_crank_nicolson_method(m, g, b0, bl, bll, ql, qll, c, dt, a, b);
+		b0 = calc_global_vector(grid.es, bind(calc_local_vector_b, _1, function_2d_t(bind(f, _1, _2, t))), grid.bn);
+		calc_crank_nicolson_method(c, m, g, b0, bl, bll, ql, qll, cs, dt, a, b);
 		set_boundary_conditions(a, b, grid.bes, t);
 		q = Eigen::JacobiSVD<matrix_t>(a, Eigen::ComputeThinU | Eigen::ComputeThinV).solve(b);
+
+		//cout << a << endl;
+		//cout << b << endl;
 
 		result.push_back(q);
 
@@ -369,14 +443,14 @@ vector<vector_t> solve_differential_equation(
 
 int main() {
 	constants_t c = {1, 1, 1, 1};
-	auto u = [] (double x, double y, double t) -> double { return x + y + t; };
+	auto u = [] (double x, double y, double t) -> double { return exp(x*x*(y-t)) + t*t; };
 	auto f = calc_right_part(u, c);
 	boundary_setter_t set_boundary_conditions = bind(write_first_boundary_conditions, _1, _2, _3, _4, u);
 
 	grid_t grid;
-	grid.calc(0, 0, 1, 1, 3, 3);
+	grid.calc(0, 0, 1, 1, 10, 10);
 
-	double at = 0, bt = 1, nt = 3;
+	double at = 0, bt = 1, nt = 10;
 	double dt = (bt-at)/(nt-1);
 
 	vector_t q0 = calc_true_approx(bind(u, _1, _2, at), grid.bes);
@@ -388,6 +462,7 @@ int main() {
 	double norm = (q-res.back()).norm();
 
 	cout << "norm: " << norm << endl;
+	cout << res.back() << endl;
 
 	system("pause");
 }
